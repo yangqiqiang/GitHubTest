a5dcd5e9bfe2666ee507b3ff0e4d0d2ee3298f1b
Ariel <294237408@qq.com>
2017-07-23 10:19:57 +0800

    this is a test project!!

--- GitHubTest/app/src/main/java/itcast/githubtest/MainActivity.java
+++ GitHubTest/app/src/main/java/itcast/githubtest/MainActivity.java
@@ -3,11 +3,15 @@ package itcast.githubtest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import itcast.githubtestlibrary.GitHubTest;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new GitHubTest();
+        //11111111111111111111111
     }
 }
